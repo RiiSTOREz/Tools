0f0714994804753bba501971500ede3297c9cf06
RiiJustHuman <fakhriajafakhriii@gmail.com>
2024-11-15 16:35:01 +0700

    Update katakata.sh

--- katakata.sh
+++ katakata.sh
@@ -1,8 +1,6 @@
 #!bin/bash
 
-echo ""
 animate_text "Halo Kaum Gamon / Sad"
-echo ""
 animate_text "Seperti Nya Anda Sangat Memikirkan Dia Dengan Yang Lain Ya? "
 animate_text "Saya Ada Kata Kata Hari Ini"
 animate_text "- Rasa Garam Itu Apa Ya?, Asing ya? "
@@ -13,7 +11,6 @@ animate_text "- Dunia Sesibuk Itu Ya? Sampe Kita Di Asingkan"
 animate_text "- nyatanya dimanapun aku berada , perananku untuk melindungimu selalu gagal. "
 animate_text "- andai dia tau aku rela ga marah dan mengesampingkan sakit hatiku, supaya kita tetap baik-baik saja."
 animate_text "- aku ga sesempurna kaya orang orang, tapi aku tau rasanya cukup dengan satu orang."
-echo ""
 echo ""
 animate_text "Mungkin Segitu Saja Dulu Kata Kata Hari Ini Moga yang lagi sedih beli golda tengah malam di indomart dengan duduk di kursi :') "
  exit
