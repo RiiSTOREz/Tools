9f0cd0c4dd6e274282f874077d41a7116f8239fe
RiiJustHuman <fakhriajafakhriii@gmail.com>
2024-11-21 01:12:55 +0700

    Update lirik.sh

--- lirik.sh
+++ lirik.sh
@@ -21,7 +21,6 @@ echo -e "Berikut Daftar Lirik Lagu Yang Tersedia :"
 echo "1. MASING MASING ( Ernie Zakri )"
 echo -e "${BLUE}[+]===============================================[+]${NC}"
 read -p "[+] PILIH LIRIK LAGU (1): " OPTION
-read -e "${BLUE}[+]===============================================[+]${NC}"
 case "$OPTION" in
      1)
       animate_text " wwwwwwwwwwwwwwwwwwwwwww"
